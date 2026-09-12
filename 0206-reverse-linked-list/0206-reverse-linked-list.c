8bb6e311be9787d6511b8e774c06952ca6890e42
Lalit Chauhan <127293503+La1itchauhan@users.noreply.github.com>
2026-09-13 01:40:50 +0530

    Time: 0 ms (100%), Space: 11.7 MB (6.35%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.c
+++ 0206-reverse-linked-list/0206-reverse-linked-list.c
@@ -1,18 +1,15 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
 struct ListNode* reverseList(struct ListNode* head) {
-    // Base case: if list is empty or has only one node
-    if (head == NULL || head->next == NULL) {
-        return head;
-    }
-    
-    // Recursively reverse the rest of the list
-    struct ListNode* reversedListHead = reverseList(head->next);
-    
-    // Reverse the pointer between the current node and the next node
+    if (head==NULL || head->next == NULL) return head;
+
+    struct ListNode *n = reverseList(head->next);
     head->next->next = head;
-    
-    // Break the original forward link to prevent a cycle
-    head->next = NULL;
-    
-    // Return the new head of the reversed list
-    return reversedListHead;
+    head->next=NULL;
+    return n;
 }
